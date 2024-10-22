bcb4c59ed63b639370c15bb2909f95b449df1071
Miltech <yengongmilton58@gmail.com>
2024-10-22 10:54:10 +0100

    using the global varaiable in a view file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,27 @@
-<div class="flex w-full h-full">
-    <img src="{{ asset('/storage/first_uploaded_image.jpg') }}" alt="img">
-   <form action="{{ route('upload-file') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="" style="margin-top: 20px">
-            <label for="">Upload</label>
-            <input type="file" name="image" id="">
-        </div>
-        <div style="margin:5px">
-            <button type="submit">Submit</button>
-        </div>
-   </form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $app_name }}</title>
+</head>
+<body>
 
-</div>
+    <div class="flex w-full h-full">
+        <img src="{{ asset('/storage/first_uploaded_image.jpg') }}" alt="img">
+       <form action="{{ route('upload-file') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="" style="margin-top: 20px">
+                <label for="">Upload</label>
+                <input type="file" name="image" id="">
+            </div>
+            <div style="margin:5px">
+                <button type="submit">Submit</button>
+            </div>
+       </form>
+
+    </div>
+
+</body>
+</html>
