6720803a6f39c9c1d2c96686379cfcffedc46198
Miltech <yengongmilton58@gmail.com>
2024-10-23 12:56:24 +0100

    addded laravel breeze for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ImageController;
-use App\Mail\Order;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,57 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group([
-        'middleware' => ['authcheck'],
-        'prefix' => 'user',
-        'as' => 'user.'
-    ], function(){
-        Route::get('/home', HomeController::class);
-        Route::post('/upload-file', [ImageController::class, 'handleImage'])->name('upload-file');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('send-mail', function(){
-   /*  Mail::raw('This is a test mail, to test sending mails', function($message){
-        $message->to('test@gmail.com')->subject('No reply');
-    }); */
-
-    Mail::send(new Order);
-
-    dd('Mail sent successfully');
-});
-
-Route::get('get-session', function(Request $request){
-    /* $data = session()->all(); */
-
-    $data = $request->session()->all();
-
-    //$data = $request->session()->get('_token');
-
-    dd($data);
-});
-
-Route::get('save-session', function(Request $request){
-    $request->session()->put('user_id', '#452er45');
-    $request->session()->put(['status' => 'authenticated', 'type' => 'admin']);
-    session(['number' => '675243564']);
-    return redirect('get-session');
-});
-
-Route::get('destroy-session-data', function(Request $request){
-    //$request->session()->forget('type');
-    //session()->forget(['status', 'number']);
-    //session()->flush();
-    $request->session()->flush();
-    return redirect('get-session');
-});
-
-Route::get('flash-session', function(Request $request){
-    //$request->session()->flash('status', 'true');
-    session()->flash('status', 'true');
-    return redirect('get-session');
-});
-
-Route::get('forget-cache', function(){
-    Cache::forget('categories');
-    return redirect('user/home?auth=1');
-});
+require __DIR__.'/auth.php';
